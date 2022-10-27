69672eebcac8ad2bdda16531767b4b5293fe6ab7
vlad <vladlipchey05@gmail.com>
2022-10-27 18:55:08 +0300

    feat: add normal session with usr data to display

--- includes/signin.php
+++ includes/signin.php
@@ -5,7 +5,15 @@
     $pass=md5($_POST['pass']);
     $check_usr=mysqli_query($connect,"SELECT * FROM `users` WHERE `login`='$login' AND `password`='$pass' ");
     if(mysqli_num_rows($check_usr)>0){
-
+        //PARSE CHECK USER INTO NORMAL ARRAY
+        $user=mysqli_fetch_assoc($check_usr);
+        $_SESSION['user']=[
+            'id' => $user['id'],
+            'full_name' => $user['full-name'],
+            'email' => $user['email'],
+            'avatar' => $user['avatar'],
+            'login' => $user['login']
+        ];
     }
     else{
         $_SESSION['msg']="WRONG LOGIN OR PASSWORD";

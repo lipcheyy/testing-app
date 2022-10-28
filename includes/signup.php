26348b9a05112d5b799ad2c870f03d00495a82f6
vlad <vladlipchey05@gmail.com>
2022-10-29 01:28:00 +0300

    feat: ready to merge

--- includes/signup.php
+++ includes/signup.php
@@ -22,39 +22,31 @@ if (mysqli_num_rows($check_login) > 0) {
     echo json_encode($response);
     die();
 }
-
-$error_fields = [];
-
+$errors = [];
 if (trim($login) == '') {
-    $error_fields[] = 'login';
+    $errors[] = 'login';
 }
-
-if ($password === '') {
-    $error_fields[] = 'password';
+if (trim($password) === '') {
+    $errors[] = 'password';
 }
-
-if ($full_name === '') {
-    $error_fields[] = 'full_name';
+if (trim($full_name) === '') {
+    $errors[] = 'full_name';
 }
-
-if ($email === '' || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
-    $error_fields[] = 'email';
+if (trim($email) === '' || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    $errors[] = 'email';
 }
-
-if ($password_confirm === '') {
-    $error_fields[] = 'password_confirm';
+if (trim($password_confirm) === '') {
+    $errors[] = 'password_confirm';
 }
-
 if (!$_FILES['avatar']) {
-    $error_fields[] = 'avatar';
+    $errors[] = 'avatar';
 }
-
-if (!empty($error_fields)) {
+If (!empty($errors)) {
     $response = [
         "status" => false,
         "type" => 1,
-        "message" => "Проверьте правильность полей",
-        "fields" => $error_fields
+        "message" => "CHECK THE FIELDS CONTENT",
+        "fields" => $errors
     ];
 
     echo json_encode($response);
@@ -69,7 +61,7 @@ if ($password === $password_confirm) {
         $response = [
             "status" => false,
             "type" => 2,
-            "message" => "Ошибка при загрузке аватарки",
+            "message" => "ERROR LOADING AVATAR",
         ];
         echo json_encode($response);
     }
@@ -88,105 +80,9 @@ if ($password === $password_confirm) {
 } else {
     $response = [
         "status" => false,
-        "message" => "Пароли не совпадают",
+        "message" => "PASSWORDS ARE NOT SAME",
     ];
     echo json_encode($response);
 }
 
 ?>
-<?php
-/*
-session_start();
-require_once 'connect.php';
-
-$full_name = $_POST['full_name'];
-$login = $_POST['login'];
-$email = $_POST['email'];
-$password = $_POST['password'];
-$password_confirm = $_POST['password_confirm'];
-
-/** @var TYPE_NAME $connect */
-/*$check_login = mysqli_query($connect, "SELECT * FROM `users` WHERE `login` = '$login'");
-if (mysqli_num_rows($check_login) > 0) {
-    $response = [
-        "status" => false,
-        "type" => 1,
-        "message" => "Такой логин уже существует",
-        "fields" => ['login']
-    ];
-
-    echo json_encode($response);
-    die();
-}
-
-$error_fields = [];
-
-if ($login === '') {
-    $error_fields[] = 'login';
-}
-
-if ($password === '') {
-    $error_fields[] = 'password';
-}
-
-if ($full_name === '') {
-    $error_fields[] = 'full_name';
-}
-
-if ($email === '' || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
-    $error_fields[] = 'email';
-}
-
-if ($password_confirm === '') {
-    $error_fields[] = 'password_confirm';
-}
-
-if (!$_FILES['avatar']) {
-    $error_fields[] = 'avatar';
-}
-
-if (!empty($error_fields)) {
-    $response = [
-        "status" => false,
-        "type" => 1,
-        "message" => "Проверьте правильность полей",
-        "fields" => $error_fields
-    ];
-
-    echo json_encode($response);
-
-    die();
-}
-
-if ($password === $password_confirm) {
-
-    $path = 'uploads/' . time() . $_FILES['avatar']['name'];
-    if (!move_uploaded_file($_FILES['avatar']['tmp_name'], '../' . $path)) {
-        $response = [
-            "status" => false,
-            "type" => 2,
-            "message" => "Ошибка при загрузке аватарки",
-        ];
-        echo json_encode($response);
-    }
-
-    $password = md5($password);
-
-    mysqli_query($connect, "INSERT INTO `users` (`id`, `full_name`, `login`, `email`, `password`, `avatar`) VALUES (NULL, '$full_name', '$login', '$email', '$password', '$path')");
-
-    $response = [
-        "status" => true,
-        "message" => "Регистрация прошла успешно!",
-    ];
-    echo json_encode($response);
-
-
-} else {
-    $response = [
-        "status" => false,
-        "message" => "Пароли не совпадают",
-    ];
-    echo json_encode($response);
-}
-
-*/?>
